10f91e5a473412abdc6ae0ade33a589808b09d94
mengo329 <megamayo3@gmail.com>
2024-12-09 21:59:26 -0500

    gfwwasaa

--- scenes/objects/level_objects/screentrigger.gd
+++ scenes/objects/level_objects/screentrigger.gd
@@ -6,18 +6,22 @@ class_name ScreenChange
 @export var scrollY1 : int
 @export var scrollY2 : int
 
-@export var direction : int
-#right = 1
-#down = 2
-#left = 3
-#up = 4
+var icon
+var oldposx
+var oldposy
+
+var oldscalex
+var oldscaley
+
+@export var direction : int ##right = 1 down = 2 left = 3 up = 4
+@export var solid : bool ##turns solid after transition
 
 
 
 @export var screenmode : int
 
 func _on_body_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") && GameState.transdir == 0:
 		GameState.scrollX1 = scrollX1
 		GameState.scrollX2 = scrollX2
 		GameState.scrollY1 = scrollY1
@@ -26,3 +30,10 @@ func _on_body_body_entered(body):
 		GameState.screenmode = screenmode
 		GameState.transdir = direction
 		GameState.screentransiton = 25
+
+
+
+func _on_body_body_exited(body):
+	if body.is_in_group("player") && solid == true:
+		$StaticBody2D/CollisionShape2D.disabled = false
+	
