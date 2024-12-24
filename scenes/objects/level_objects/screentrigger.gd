45ac1d1a89daa67bbaca1f0fc5d8198fee066094
mengo329 <megamayo3@gmail.com>
2024-12-24 00:07:03 -0500

    geeg

--- scenes/objects/level_objects/screentrigger.gd
+++ scenes/objects/level_objects/screentrigger.gd
@@ -17,14 +17,17 @@ var oldscaley
 
 @export var screenmode : int
 
+@export var ladderonly : bool
+
 func _on_body_entered(body):
 	print("yo")
 	if body.is_in_group("player") && GameState.transdir == 0:
-		GameState.scrollX1 = scrollX1
-		GameState.scrollX2 = scrollX2
-		GameState.scrollY1 = scrollY1
-		GameState.scrollY2 = scrollY2
-
-		GameState.screenmode = screenmode
-		GameState.transdir = direction
-		GameState.screentransiton = 25
+		if ladderonly == false or GameState.playerstate == 7:
+			GameState.scrollX1 = scrollX1
+			GameState.scrollX2 = scrollX2
+			GameState.scrollY1 = scrollY1
+			GameState.scrollY2 = scrollY2
+	
+			GameState.screenmode = screenmode
+			GameState.transdir = direction
+			GameState.screentransiton = 25
